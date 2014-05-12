8c78a668c25e4159f77006d490d86c92f0542f6e
Jakob Blomer <jblomer@cern.ch>
2014-05-12 18:03:13 +0200

    re-enable after migration

--- next_build.sh
+++ next_build.sh
@@ -3,7 +3,7 @@ set -e
 
 DEST_REPOSITORY=${DEST_REPOSITORY:=cernvm-slc5.cern.ch}
 
-createrepo -d -s sha /data/yum/cernvm/extras/5/x86_64 --workers=12
+#createrepo -d -s sha /data/yum/cernvm/extras/5/x86_64 --workers=12
 touch _refetch_repometadata
 make
 make -f install.mk DEST_REPOSITORY="$DEST_REPOSITORY"
