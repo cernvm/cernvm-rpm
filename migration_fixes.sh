9db6f731c6aec34616092ee6a76654af6d5bfcb5
Jakob Blomer <jblomer@cern.ch>
2014-11-04 23:04:33 +0100

    security fixes post RHEL6.6

--- migration_fixes.sh
+++ migration_fixes.sh
@@ -5,17 +5,11 @@ YUM_OPTIONS="$2"
 
 sudo package-cleanup $YUM_OPTIONS --oldkernels --count=1
 
-if rpm --root "$DEST_ROOT" -q gcutil; then
-  sudo yum $YUM_OPTIONS erase gcutil
-fi
-
-if rpm --root "$DEST_ROOT" -q gsutil; then
-  sudo yum $YUM_OPTIONS erase gsutil
-fi
-
-if rpm --root "$DEST_ROOT" -q retry_decorator; then
-  sudo yum $YUM_OPTIONS erase retry_decorator
-fi
+for pkg in java-1.5.0-gcj java_cup sinjdoc gcutil gsutil retry_decorator; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
 
 # SL6.4 --> SL6.5
 #if rpm --root "$DEST_ROOT" -q tigervnc-server; then
--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -3,6 +3,10 @@
 DEST_ROOT="$1"
 YUM_OPTIONS="$2"
 
+#if rpm --root "$DEST_ROOT" -q xorg-x11-drv-void; then
+#  sudo yum $YUM_OPTIONS erase xorg-x11-drv-void
+#fi
+
 if rpm --root "$DEST_ROOT" -q openafs-kpasswd; then
   sudo yum $YUM_OPTIONS erase openafs-kpasswd
 fi
