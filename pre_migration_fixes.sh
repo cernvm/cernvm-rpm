d4d3b933120a4084d7cb73d9e9786892976a5db7
Jakob Blomer <jblomer@cern.ch>
2014-12-15 17:10:12 +0100

    adjust after latest security fixes

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -3,9 +3,9 @@
 DEST_ROOT="$1"
 YUM_OPTIONS="$2"
 
-#if rpm --root "$DEST_ROOT" -q xorg-x11-drv-void; then
-#  sudo yum $YUM_OPTIONS erase xorg-x11-drv-void
-#fi
+if rpm --root "$DEST_ROOT" -q xorg-x11-drv-void; then
+  sudo yum $YUM_OPTIONS erase xorg-x11-drv-void
+fi
 
 if rpm --root "$DEST_ROOT" -q openafs-kpasswd; then
   sudo yum $YUM_OPTIONS erase openafs-kpasswd
