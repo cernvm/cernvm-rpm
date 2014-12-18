ecd0bdef6d8c9611a5f24eea59ed06e9e80624cd
Jakob Blomer <jblomer@cern.ch>
2014-12-18 11:33:19 +0100

    bump version

--- migration_fixes.sh
+++ migration_fixes.sh
@@ -5,22 +5,9 @@ YUM_OPTIONS="$2"
 
 sudo package-cleanup $YUM_OPTIONS --oldkernels --count=1
 
-if rpm --root "$DEST_ROOT" -q gcutil; then
-  sudo yum $YUM_OPTIONS erase gcutil
-fi
-
-if rpm --root "$DEST_ROOT" -q gsutil; then
-  sudo yum $YUM_OPTIONS erase gsutil
-fi
-
-if rpm --root "$DEST_ROOT" -q retry_decorator; then
-  sudo yum $YUM_OPTIONS erase retry_decorator
-fi
-
-# SL6.4 --> SL6.5
-#if rpm --root "$DEST_ROOT" -q tigervnc-server; then
-#  if rpm --root "$DEST_ROOT" -q tigervnc-server-module; then
-#    sudo yum $YUM_OPTIONS erase tigervnc-server-module
-#  fi
-#fi
+for pkg in gcutil gsutil retry_decorator; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
 
