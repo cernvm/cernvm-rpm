41acb1d7721b03c37722e838d18c545f4f5b1ee1
Jakob Blomer <jblomer@cern.ch>
2015-01-22 16:13:21 +0100

    adjustments for server 2.1.20

--- set_rolling_tag.sh
+++ set_rolling_tag.sh
@@ -7,7 +7,7 @@ ROLLING_TAG=HEAD
 [ -z "$CVMFS_REPOSITORY" ] && exit 1
 [ -z "$VERSION" ] && exit 1
 
-HASH=$(sudo cvmfs_server lstags ${CVMFS_REPOSITORY} | grep cernvm-system-${VERSION} | awk -F'|' '{print $2}' | sed "s/ //")
+HASH=$(sudo cvmfs_server tag -lx ${CVMFS_REPOSITORY} | grep cernvm-system-${VERSION} | awk '{print $2}' | sed "s/ //")
 sudo cvmfs_server transaction ${CVMFS_REPOSITORY}
 sudo cvmfs_server publish -a ${ROLLING_TAG} -h ${HASH} ${CVMFS_REPOSITORY}
 sudo cvmfs_server lstags ${CVMFS_REPOSITORY}
