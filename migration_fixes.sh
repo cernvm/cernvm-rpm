af8bfaf1370c0901d29cacff09bf601cf54e68ce
Jakob Blomer <jblomer@cern.ch>
2015-05-29 13:18:26 +0200

    3.4 testing towards finish

--- migration_fixes.sh
+++ migration_fixes.sh
@@ -5,10 +5,10 @@ YUM_OPTIONS="$2"
 
 sudo package-cleanup $YUM_OPTIONS --oldkernels --count=1
 
-#for pkg in java-1.5.0-gcj java_cup sinjdoc gcutil gsutil retry_decorator ruby-rgen \
-#  copilot-agent copilot-jobmanager-generic copilot-util perl-Copilot perl-Copilot-Component-Agent \
-#  perl-Copilot-Component-KeyManager perl-Copilot-Component-JobManager-Generic perl-Copilot-Component-ContextAgent; do
-for pkg in java-1.5.0-gcj java_cup sinjdoc gcutil gsutil retry_decorator ruby-rgen; do
+for pkg in java-1.5.0-gcj java_cup sinjdoc gcutil gsutil retry_decorator ruby-rgen \
+  copilot-agent copilot-jobmanager-generic copilot-util perl-Copilot perl-Copilot-Component-Agent \
+  perl-Copilot-Component-KeyManager perl-Copilot-Component-JobManager-Generic perl-Copilot-Component-ContextAgent; do
+#for pkg in java-1.5.0-gcj java_cup sinjdoc gcutil gsutil retry_decorator ruby-rgen; do
   if rpm --root "$DEST_ROOT" -q $pkg; then
     sudo yum $YUM_OPTIONS erase $pkg
   fi
