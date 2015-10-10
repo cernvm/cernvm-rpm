06afdc7142c1770784217899b15b6c9d3ffdfeb5
Jakob Blomer <jblomer@cern.ch>
2015-10-10 16:48:07 +0000

    some additions and fixes

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -3,3 +3,9 @@
 DEST_ROOT="$1"
 YUM_OPTIONS="$2"
 
+for pkg in cronie cronie-noanacron cloud-utils-growpart liberation-serif-fonts; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
+
