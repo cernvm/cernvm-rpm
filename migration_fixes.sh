633784e975349944c73781808f14a5af024cd102
Jakob Blomer <jblomer@cern.ch>
2016-02-19 14:29:24 +0000

    add phinx

--- migration_fixes.sh
+++ migration_fixes.sh
@@ -5,9 +5,9 @@ YUM_OPTIONS="$2"
 
 sudo package-cleanup $YUM_OPTIONS --oldkernels --count=1
 
-#for pkg in slim; do
-#  if rpm --root "$DEST_ROOT" -q $pkg; then
-#    sudo yum $YUM_OPTIONS erase $pkg
-#  fi
-#done
+for pkg in geoclue NetworkManager-glib cronie-anacron; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
 
