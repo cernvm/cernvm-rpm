2440a8c9968a3738ea55c1bb1f676247cf9a3586
Jakob Blomer <jblomer@cern.ch>
2016-03-17 15:25:08 +0000

    add packages

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -3,9 +3,9 @@
 DEST_ROOT="$1"
 YUM_OPTIONS="$2"
 
-for pkg in libunwind cloud-utils-growpart; do
-  if rpm --root "$DEST_ROOT" -q $pkg; then
-    sudo yum $YUM_OPTIONS erase $pkg
-  fi
-done
+#for pkg in libunwind cloud-utils-growpart; do
+#  if rpm --root "$DEST_ROOT" -q $pkg; then
+#    sudo yum $YUM_OPTIONS erase $pkg
+#  fi
+#done
 
