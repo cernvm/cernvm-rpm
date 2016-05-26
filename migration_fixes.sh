58a21630aef66caf896d3019d8abf9ff6fb52ed8
Jakob Blomer <jblomer@cern.ch>
2016-05-26 07:31:24 +0000

    add packages

--- migration_fixes.sh
+++ migration_fixes.sh
@@ -5,9 +5,9 @@ YUM_OPTIONS="$2"
 
 sudo package-cleanup $YUM_OPTIONS --oldkernels --count=1
 
-#for pkg in geoclue NetworkManager-glib cronie-anacron; do
-#  if rpm --root "$DEST_ROOT" -q $pkg; then
-#    sudo yum $YUM_OPTIONS erase $pkg
-#  fi
-#done
+for pkg in m2crypto polkit-gnome compat-libupower-glib1 compat-libcogl12; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
 
