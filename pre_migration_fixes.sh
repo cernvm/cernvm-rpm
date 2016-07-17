9bb1e6419911c7fc2c96125628faa754cad2acfd
Jakob Blomer <jblomer@cern.ch>
2016-07-17 23:04:25 +0000

    add qt, xerces-c

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -3,6 +3,8 @@
 DEST_ROOT="$1"
 YUM_OPTIONS="$2"
 
+#sudo yum $YUM_OPTIONS erase $(rpm --root $DEST_ROOT -q '*-i686')
+
 #for pkg in libunwind cloud-utils-growpart; do
 #  if rpm --root "$DEST_ROOT" -q $pkg; then
 #    sudo yum $YUM_OPTIONS erase $pkg
