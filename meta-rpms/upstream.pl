131eb7820214b685c9b857244242412d6e2bdf3f
Felix Scheffler <felixscheffler@moonshot-arm64-03.cern.ch>
2016-08-17 10:36:23 +0200

    Add packages

--- meta-rpms/upstream.pl
+++ meta-rpms/upstream.pl
@@ -18,3 +18,21 @@
   'extras' => 0,
   'active' => 1,
 },
+'cvmfs' => {
+  'baseurl' => {
+    'el7' => {
+      'aarch64' => 'http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs-testing/EL/7/aarch64',
+    },
+  },
+  'extras' => 0,
+  'active' => 1,
+},
+'cvmextras' => {
+  'baseurl' => {
+    'el7' => {
+      'aarch64' => 'http://localhost/data/yum/cernvm/extras/7/aarch64',
+    }
+  },
+  'extras' => 1,
+  'active' => 1,
+},
--- next_build.sh
+++ next_build.sh
@@ -4,8 +4,8 @@ set -e
 DEST_REPOSITORY=${DEST_REPOSITORY:=cernvm-aarch64.cern.ch}
 
 #make -C "$RENEGADE_PATH"
-#touch _refetch_repometadata
-#make
+touch _refetch_repometadata
+make
 make -f install.mk DEST_REPOSITORY="$DEST_REPOSITORY"
 make -f archive.mk
 
