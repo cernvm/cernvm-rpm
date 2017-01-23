188be7afbb87732affb639581e63e6270e407a9a
Jakob Blomer <jblomer@cern.ch>
2017-01-23 10:13:16 +0100

    explicitly add openblas

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -5,9 +5,9 @@ YUM_OPTIONS="$2"
 
 #sudo yum $YUM_OPTIONS erase $(rpm --root $DEST_ROOT -q '*-i686')
 
-#for pkg in python-jinja2  cloud-utils-growpart; do
-#  if rpm --root "$DEST_ROOT" -q $pkg; then
-#    sudo yum $YUM_OPTIONS erase $pkg
-#  fi
-#done
+for pkg in mono-nunit mono-nunit-devel mono-data-postgresql; do
+  if rpm --root "$DEST_ROOT" -q $pkg; then
+    sudo yum $YUM_OPTIONS erase $pkg
+  fi
+done
 
