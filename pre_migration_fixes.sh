706b95e6255e644db25581bb1b0b767107e40358
Jakob Blomer <jblomer@cern.ch>
2017-02-21 22:25:39 +0100

    new packages

--- pre_migration_fixes.sh
+++ pre_migration_fixes.sh
@@ -5,9 +5,15 @@ YUM_OPTIONS="$2"
 
 #sudo yum $YUM_OPTIONS erase $(rpm --root $DEST_ROOT -q '*-i686')
 
-for pkg in mono-nunit mono-nunit-devel mono-data-postgresql; do
-  if rpm --root "$DEST_ROOT" -q $pkg; then
-    sudo yum $YUM_OPTIONS erase $pkg
-  fi
-done
+#for pkg in samba-client samba-common samba-common-libs samba-client-libs samba-libs samba-common-tools pytalloc rsyslog-mmjsonparse libnfsidmap; do
+#  if rpm --root $DEST_ROOT -q $pkg; then
+#    sudo yum $YUM_OPTIONS erase $pkg
+#  fi
+#done
+
+#for pkg in mono-nunit mono-nunit-devel mono-data-postgresql; do
+#  if rpm --root "$DEST_ROOT" -q $pkg; then
+#    sudo yum $YUM_OPTIONS erase $pkg
+#  fi
+#done
 
